51a6780fc3d420740d8ce169446515779c98c78d
AriRamReiner <568943@my.cuhsd.org>
2026-09-03 11:10:52 -0700

    Improvements to running and while, and added comments!

--- basicCalculator.java
+++ basicCalculator.java
@@ -4,24 +4,23 @@ public class basicCalculator {
     public static void main(String[] args){
         //Define scanner object using "new".
         Scanner in = new Scanner(System.in);
-        boolean running = true;
         String answer;
-        while (running) {
+        while (true) {
             System.out.println("E - exit");
             System.out.println("+ - addition");
             answer = in.nextLine();
             if (answer.equals("E")){
-                running = false;
+                break; //Stop while statement.
             }
             else if (answer.equals("+")) {
-                float total = 0;
-                answer = "0";
+                float total = 0; //Define total variable.
+                answer = "0"; //Put answer in 0 to be able to turn into float.
                 while (!answer.equals("S")) {
-                    total += Float.parseFloat(answer);
+                    total += Float.parseFloat(answer); //Puts answer to total. At the start it is always 0+0.
                     System.out.print("Insert number or S to stop:  ");
                     answer = in.nextLine();
                 }
-                System.out.println(total);
+                System.out.println(total); //Print final answer.
             }
         }
     }
